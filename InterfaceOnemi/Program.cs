93049490400e8df435f0c82905fcfa4f699e2004
Onur OZBEK <20170301041@dogus.edu.tr>
2019-05-04 19:28:40 +0300

    ilk commit

---
+++ InterfaceOnemi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InterfaceOnemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILoger _loger;
+            _loger = new TextLoger();
+
+            _loger.Log();
+            Console.ReadLine();
+        }
+    }
+    public class TextLoger:ILoger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Text dosyarı Loglama yapıldı");
+        }
+    }
+    public class DatabaseLoger : ILoger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Database Loglama yapıldı");
+        }
+    }
+    public interface ILoger
+    {
+        void Log();
+    }
+}
